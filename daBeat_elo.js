a4badb2480030963f76b58dbc92a405b2a070cbd
Eloielbonnoi <57134772+Eloielbonnoi@users.noreply.github.com>
2022-12-08 16:40:24 +0100

    Add files via upload

--- daBeat_elo.js
+++ daBeat_elo.js
@@ -17,36 +17,37 @@ let hhpat = [1, 1, 1, 1, 1, 1, 1, 1]
 
 //console.log("elkick:", whichKick)
 
+//Funcions relacionades amb el lleuger desplaçament en el temps dels silencis entre els esdeveniments dels patterns al drumkit
+
+
 let arrayDelaysKck = [];
 let arrayDelaySnare = [];
 let arrayDelayHat = [];
 let arrayPreviKck = [0.93, 0.73, 0.74, 1.7, 0.83, 0.97, 0.5, 1.6]; //Suma 8. Son els delays predefinits pel Pau
 let arrayPreviSnare = [1.02, 2, 0.85, 1.15, 2, 0.85, 0.13];
 let arrayPreviHat = [0.52, 0.48, 0.52, 0.48]
-let valor;
 
 let seed1 = 2; // seed és una variable que hem definit previament. Poso 2 per posar algun valor
 let ck = seed1 * 4;
 let seed2 = 3;
 let cs = seed2 * 4;
-let cx = seed1 * 6 
+let cx = seed1 * 6
 let factorRandomKick = getRandomArbitrary(-0.01, 0.01) * ck;
 let factorRandomSnare = getRandomArbitrary(-0.02, 0.02) * cs;
 let factorRandomHat = getRandomArbitrary(0, 0.03) * cx;
 
 //Funció per determinar valors dels silencis de qualsevol array. Reb un array i el factor de randomització i retorna un array nou
 
-function valorsSleep (array, factor){
-    nouArray = array.map(function (num, i){
+function valorsSleep(array, factor) {
+    nouArray = array.map(function (num, i) {
         if (i % 2 === 0) { num = num + factor } else { num = num - factor }
-        return num  
+        return num
     })
     return nouArray;
 }
 
 // arrayDelaysKck = valorsSleep(arrayPreviKck, factorRandomKick);
 // console.log(arrayDelaysKck);
-
 // arrayDelaysHats = valorsSleep(arrayPreviHat, factorRandomHat);
 
 
