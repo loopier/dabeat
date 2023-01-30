e157be155d721a8cc193489039e39482764af137
Eloielbonnoi <57134772+Eloielbonnoi@users.noreply.github.com>
2023-01-30 15:43:32 +0100

    Add files via upload

---
+++ lyrics-filenames.js
@@ -0,0 +1,10 @@
+const lyrics = [
+    "Coky.wav",
+    "kaseoyemen.wav",
+    "KnightRider.wav",
+    "NAS290.wav",
+    "puercos7notas.wav",
+    "X2Download-Jay-Z-Threat-Acapella-BPM-90.wav",
+    "X2Download-Nipsey-Hussle-Ft-Rick-Ross-Mark-My-Words-90-BPM.wav",
+    "X2Download-Zabaz-Grease-Ft-Brain-Bonaparte-(ACAPELLA-90BPM)-(FUNKDEALER).wav",
+];
